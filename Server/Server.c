56081d9546bf3044342ee712b393a8f1a9aab0dc
salvaore31 <45175715+salvaore31@users.noreply.github.com>
2019-07-03 18:43:30 +0200

    Funzione checkUsername e rettifica costante USERS_FILE

--- Server/Server.c
+++ Server/Server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -10,28 +11,28 @@
 
 int main(){
 
-  return 0;
+  return 1;
 }
-
+/*checkUsername FUNZIONA BISOGNA GESTIRE IL COMPORTAMENTO IN CASO DI ERRORE NELL' APERTURA*/
 int checkUsername(char* username){
-  int fdUserFile, i=0, res,r;
+  int fdUserFile, i=0, res,r=1;
   char c, str[MAX_SIZE_USERNAME];
-  if(fdUserFile=open(USERS_FILE,O_RDONLY)<0||fdUserFile==1){
+  if((fdUserFile=open(USERS_FILE,O_RDONLY))<0){
     printf("errore\n" );
     //GESTIRE ERRORE APERTURA FILE
   }else{
-    while(read(fdUserFile,&c,1)>0){
-      printf("%d%c",fdUserFile,c);
-      if(c!='%')
+    while(r>0){
+      while(((r=read(fdUserFile,&c,1))>0)&&c!='%')
         str[i++]=c;
       str[i++]='\0';
-      printf("ohhh%s\n",str);
       if(strcmp(str,username)==0){
         res=lseek(fdUserFile,0,SEEK_CUR);
+        lseek(fdUserFile,34,SEEK_CUR);
+        read(fdUserFile,&c,1);
         close(fdUserFile);
-        return res;
+        return res+1;
       }
-      while(((r=read(fdUserFile,&c,1))>0)&&c=='\n');
+      while(((r=read(fdUserFile,&c,1))>0)&&c!='\n');
       i=0;
     }
     close(fdUserFile);
--- costanti.h
+++ costanti.h
@@ -3,7 +3,7 @@
 
 //TODO CAPIRE COME SI DEFINISCE UN VALORE INTERO POSITIVO A 16 bit e metterli dove ha senso
 #define MAX_SIZE_USERNAME 15
-#define USERS_FILE "File\\RegisteredUser.txt"
+#define USERS_FILE "File/RegisteredUser.txt"
 /* Le costanti seguenti corrispondono ai valori da usare come secondo operando
  * di un AND bit a bit con il valore corrente di una casella della matrice
  * di gioco per ottenere informazioni sullo stato della casella.
