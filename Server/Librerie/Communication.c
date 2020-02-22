968139479a4f0968e902ce942774852943cc38b3
salvaore31 <45175715+salvaore31@users.noreply.github.com>
2020-02-22 11:43:08 +0100

    Non ho fatto casini spero

--- Server/Librerie/Communication.c
+++ Server/Librerie/Communication.c
@@ -61,7 +61,7 @@ int sendMsgNoReply(int sockfd,char toSend[]){
   char msg[150];
 
   clear();
-  sprintf(msg,"%d",strlen(toSend));
+  sprintf(msg,"%ld",strlen(toSend));
   n_b_w=write(sockfd,msg,strlen(msg));
   if(n_b_w<strlen(msg)){
       return ERR_SENDING_MESSAGE;
@@ -83,7 +83,7 @@ int sendMsg(int sockfd,char toSend[],char received[]){
   char msg[250];
 
   clear();
-  sprintf(msg,"%d",strlen(toSend));
+  sprintf(msg,"%ld",strlen(toSend));
   n_b_w=write(sockfd,msg,strlen(msg));
   if(n_b_w<strlen(msg)){
       return ERR_SENDING_MESSAGE;
